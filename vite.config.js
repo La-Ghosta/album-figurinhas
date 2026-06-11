c4da03dbd71ff78fad022e062106670054b1ac22
Guilherme Holanda <201495863+La-Ghosta@users.noreply.github.com>
2026-06-11 18:16:00 -0300

    Configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// base = nome do repositório, necessário pro GitHub Pages
 export default defineConfig({
   plugins: [vue()],
+  base: '/album-figurinhas/'
 })
